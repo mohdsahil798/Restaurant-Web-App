cd9bda2dda53274de5a2d287eca2a62eba005b71
nisarg-ttn <nisarg.chhapre@tothenew.com>
2025-02-03 05:45:07 +0530

    added dish and category apis

--- management/serializers.py
+++ management/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Reservation
+from .models import Reservation, MealCategory, Dish
 
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
+        fields = "__all__"
+    
+class MealCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MealCategory
+        fields = "__all__"
+        
+class DishSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dish
         fields = "__all__"
--- management/urls.py
+++ management/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('api/reservations/', ReservationAPIView.as_view()),
-    path('api/reservations/<int:pk>', ReservationAPIView.as_view())
+    path('api/reservations/<int:pk>', ReservationAPIView.as_view()),
+    
+    path('api/meal-categories/', MealCategoryList.as_view(), name = "meal-category-list"),
+    path('api/meal-categories/<int:pk>/', MealCategoryDetail.as_view(), name = "meal-category-list"),
 ]
